61448458d0a86d1375cee2c4e90a23e9988dba2f
akitayuri <nmnl_y.bgux@icloud.com>
2021-05-21 14:44:40 +0900

    fix:タイムゾーン修正

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.time_zone = "Tokyo"
-config.active_record.default_timezone = :local
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,6 +10,8 @@ module TwitterClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
