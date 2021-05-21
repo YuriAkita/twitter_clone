5721ecd95b9aa246d4af5ba6dcc449df8f037aab
akitayuri <nmnl_y.bgux@icloud.com>
2021-05-21 13:17:07 +0900

    add:createメソッドの作成、投稿作成機能、リダイレクト機能、通知機能追加。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,16 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-end
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to new_post_path,notice: "つぶやきが作成されました！"
+      else
+        render :new
+      end
+    end
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts do
+  end
   get '/', to: 'posts#index'
 end
